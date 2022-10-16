85b1223361642c73139e2bac676ead469c68a2e1
Henrique Malikovski <hmalikovski@gmail.com>
2022-10-16 18:50:37 -0300

    Progresso da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { useState } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
-import { GlobalStyle } from './styles/global';
-import { defaultTheme } from './styles/theme/default';
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary' />
-      <Button variant='secondary' />
-      <Button variant='success' />
-      <Button variant='danger' />
-      <Button />
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
-  );
+  )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
     *{
@@ -15,10 +15,11 @@ export const GlobalStyle = createGlobalStyle`
     body{
         background-color: ${(props) => props.theme['gray-900']};
         color: ${(props) => props.theme['gray-300']};
+        -webkit-font-smoothing: antialiased;
     }
     body, input textarea, button{
         font-family: 'Roboto', sans-serif;
         font-weight: 400;
         font-size: 1rem;
     }
-`;
+`
